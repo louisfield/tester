bc203bb92f8e0031cbeae89eb6c59195e1808bc1
louisfield <lf00341@surrey.ac.uk>
2024-04-22 20:58:50 +0100

    random css playing

--- lib/tester_web/liveview/products.ex
+++ lib/tester_web/liveview/products.ex
@@ -5,22 +5,12 @@ defmodule TesterWeb.LiveView.Products do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <h1>Products</h1>
-      <ul>
-        <div
-          :for={product <- @products}
-          href="#"
-          class="col-auto grid grid-cols-4 gap-4 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-fit"
-        >
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            <%= product.name %>
-          </h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400">
-            <%= product.price %>
-          </p>
-        </div>
-      </ul>
+    <div class=" w-screen h-screen bg-slate-700">
+      <div class="w-100 h-96 grid grid-rows-3 grid-flow-col gap-4 flex flex-row ">
+        <div class="row-span-3 rounded-lg shadow-lg bg-violet-950">01</div>
+        <div class="col-span-2 rounded-lg shadow-lg bg-violet-950">02</div>
+        <div class="row-span-2 col-span-2 rounded-lg shadow-lg bg-violet-950">03</div>
+      </div>
     </div>
     """
   end
